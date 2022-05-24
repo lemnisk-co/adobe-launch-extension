4c6b7876c5c4b53d7004b1f37ca5ae53aaa88262
Praveen DS <--unset>
2022-05-24 18:15:33 +0530

    Removed console.log comments

--- src/lib/actions/identify.js
+++ src/lib/actions/identify.js
@@ -3,8 +3,6 @@
 var lmSMTObj = require('./helpers/lem');
 
 module.exports = function(settings) {
-    console.log("In Identify");
-    console.log(settings);
     lmSMTObj.identify(settings.g_id.visitor_id, settings.g_props, settings.g_other_ids)
     turbine.logger.log('Send event: identify(' + JSON.stringify(settings) + ')');
 };
--- src/lib/actions/track.js
+++ src/lib/actions/track.js
@@ -2,8 +2,6 @@
 var lmSMTObj = require('./helpers/lem');
 
 module.exports = function (settings) {
-    console.log("In Track");
-    console.log(settings);
     lmSMTObj.track(settings.g_event.event, settings.g_props, settings.g_id);
     turbine.logger.log('Send event: Track(' + JSON.stringify(settings) + ')');
 };
